57b81e2258899b36054723406336a504a303959e
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2016-10-01 03:45:22 -0300

    Ideas de como encarar el desarrollo

--- encontrar texto/main.py
+++ encontrar texto/main.py
@@ -1,1 +1,17 @@
+import sys
 
+def procesar_renglon(cadena):
+    """Esta funcion toma una cadena de texto y procesa los datos y los devuelve
+    en alguna estructura"""
+    pass
+
+def recorrer_archivo(archivo, func):
+    """Esta funcion recorre el archivo de texto que se le pasa y a cada renglon
+    le aplica la funcion de argumento"""
+    pass
+
+def procesar_datos(func):
+    """Esta funcion maneja los datos obtenidos, procesados por la funcion argumento"""
+    pass
+
+nombre_del_archivo = sys.argv[1] #argumentos pasados por consola [0] es el script, [1] el archivo de texto
